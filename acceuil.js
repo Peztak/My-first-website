ce0fe71ea78c46aadf5e3d4b93123e99c892dbc8
Peztak <92151905+Peztak@users.noreply.github.com>
2021-10-30 18:48:53 +0200

    Add files via upload

--- acceuil.js
+++ acceuil.js
@@ -16,27 +16,27 @@ var btnPopup2 = document.getElementById('btnPopup2');
 var overlay2 = document.getElementById('overlay2');
 var btnClose2 = document.getElementById('btnClose2');
 
-btnPopup2.addEventListener('click',openModal);
+btnPopup2.addEventListener('click',openModal2);
 
-btnClose2.addEventListener('click',closePopup);
+btnClose2.addEventListener('click',closePopup2);
 
-function openModal() {
+function openModal2() {
 	overlay2.style.display='block';
 }
-function closePopup() {
+function closePopup2() {
 	overlay2.style.display='none';
 }
 var btnPopup3 = document.getElementById('btnPopup3');
 var overlay3 = document.getElementById('overlay3');
 var btnClose3 = document.getElementById('btnClose3');
 
-btnPopup3.addEventListener('click',openModal);
+btnPopup3.addEventListener('click',openModal3);
 
-btnClose3.addEventListener('click',closePopup);
+btnClose3.addEventListener('click',closePopup3);
 
-function openModal() {
+function openModal3() {
 	overlay3.style.display='block';
 }
-function closePopup() {
+function closePopup3() {
 	overlay3.style.display='none';
 }
